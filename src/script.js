c9f042dfb75a9f62549d04d7bd1e9d7632f52866
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2018-10-21 15:58:48 -0500

    initial commit

---
+++ src/script.js
@@ -0,0 +1,12 @@
+let fbArray = new Array ();
+
+for (let i=1; i<=100; i++) {
+  fbArray.push(FizzBuzz(i));
+}
+console.log(fbArray);
+
+function FizzBuzz(numberIn){
+  let result = numberIn;
+
+  return result
+}
